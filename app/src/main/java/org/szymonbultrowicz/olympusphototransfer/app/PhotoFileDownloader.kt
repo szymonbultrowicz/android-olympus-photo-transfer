0e38b53c19ec78a97ed87be3d9c13ee33434f4f0
Szymon Bultrowicz <szymonbultrowicz@users.noreply.github.com>
2019-11-24 02:25:11 +0100

    fix: Missing date taken exif data

--- app/src/main/java/org/szymonbultrowicz/olympusphototransfer/app/PhotoFileDownloader.kt
+++ app/src/main/java/org/szymonbultrowicz/olympusphototransfer/app/PhotoFileDownloader.kt
@@ -2,6 +2,7 @@ package org.szymonbultrowicz.olympusphototransfer.app
 
 import android.content.ContentResolver
 import android.content.ContentValues
+import android.media.ExifInterface
 import android.os.Build
 import android.provider.MediaStore
 import kotlinx.coroutines.Dispatchers
@@ -9,8 +10,13 @@ import kotlinx.coroutines.withContext
 import org.szymonbultrowicz.olympusphototransfer.lib.client.CameraClient
 import org.szymonbultrowicz.olympusphototransfer.lib.client.FileInfo
 import org.szymonbultrowicz.olympusphototransfer.lib.exceptions.PhotoDownloadException
+import java.text.SimpleDateFormat
+import java.time.ZoneId
+import java.util.*
 
 class PhotoFileDownloader {
+
+    val exifDateFormat: SimpleDateFormat = SimpleDateFormat("yyyy:MM:dd hh:mm:ss", Locale.getDefault())
 
     suspend fun downloadFile(
         file: FileInfo,
@@ -32,6 +38,20 @@ class PhotoFileDownloader {
             contentResolver.openOutputStream(uri).use { outputStream ->
                 if (outputStream != null) {
                     camera.downloadFile(file, outputStream)
+                }
+            }
+            if (file.extension == "jpg") {
+                contentResolver.openFileDescriptor(uri, "rw")?.use {
+                    // set Exif attribute so MediaStore.Images.Media.DATE_TAKEN will be set
+                    val takenTimestamp = file.dateTaken.atZone(ZoneId.systemDefault()).toInstant()
+                    ExifInterface(it.fileDescriptor)
+                        .apply {
+                            setAttribute(
+                                ExifInterface.TAG_DATETIME_ORIGINAL,
+                                exifDateFormat.format(Date.from(takenTimestamp))
+                            )
+                            saveAttributes()
+                        }
                 }
             }
             uri
